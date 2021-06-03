20439f359f934b710a23c5a7868d778760ae0b95
Pavel <imran.pavel@gmail.com>
2021-06-03 13:06:45 +0300

    1.3 - 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,32 +5,32 @@ import Total from './components/Total';
 import Header from './components/Header';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
 
-      <Content
-
-        part1={part1}
-        exercises1={exercises1}
-
-        part2={part2}
-        exercises2={exercises2}
-
-        part3={part3}
-        exercises3={exercises3}
-
-      />
+      <Content parts={course.parts}/>
       
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Total total={course.parts.reduce((acc, curr) => acc + curr.exercises, 0)}/>
       
     </div>
   )
