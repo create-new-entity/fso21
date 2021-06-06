7090ec94579d22b1bfdb5b9baf79cb98eb8d9ccb
Pavel <imran.pavel@gmail.com>
2021-06-06 20:30:52 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
+
   const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '34-23-3434'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ srchStr, setsrchStr ] = useState('')
 
 
   const handleSubmit = (event) => {
@@ -42,19 +45,33 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSrchStrChange = (event) => {
+    setsrchStr(event.target.value);
+  };
+
 
 
   const renderedNumbers = persons.map((person) => {
+    if(srchStr.length) {
+      if(person.name.toLowerCase().includes(srchStr.toLowerCase())) {
+        return <p key={person.name}>{person.name}   {person.number}</p>;
+      }
+      return;
+    }
     return (
       <p key={person.name}>{person.name}   {person.number}</p>
     );
   });
 
-  
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>filter shown with <input value={srchStr} onChange={handleSrchStrChange}/></p>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange}/>
