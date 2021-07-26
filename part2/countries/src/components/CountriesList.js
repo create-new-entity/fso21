2618353ec41f5af0210334a6e24f505c9c408890
Pavel <imran.pavel@gmail.com>
2021-07-27 02:40:55 +0300

    2.14

--- part2/countries/src/components/CountriesList.js
+++ part2/countries/src/components/CountriesList.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
+import axios from 'axios';
 
 import Country from './Country';
 
+const weatherBaseURL = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API}&query=`;
+
 
 const Countries = ({countries, handleShowButtonClick}) => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  
+  useEffect(() => {
+    let loadWeatherData = async () => {
+      let filteredCountries = countries.filter(country => country.show);
+      let promises = filteredCountries.map(country => axios.get(weatherBaseURL+country.capital));
+      let responses = await Promise.all(promises);
+      
+      let newWeatherData = {};
+      responses.forEach(response => {
+        newWeatherData[response.data.location.country.toLowerCase()] = {
+          location: response.data.location.name,
+          temperature: response.data.current.temperature,
+          wind: response.data.current.wind_speed,
+          wind_direction: response.data.current.wind_dir,
+          icon: response.data.current.weather_icons[0]
+        };
+      });
+      
+      setWeatherData(newWeatherData);
+    };
+    loadWeatherData();
+  }, [countries]);
+
   let renderedCountries = countries.map(country => {
     if(!country.show) {
       return (
@@ -23,6 +51,7 @@ const Countries = ({countries, handleShowButtonClick}) => {
             population={country.population}
             languages={country.languages.map(language => language.name)}
             flag={country.flag}
+            weatherData={weatherData[country.name.toLowerCase()]}
           />
         </div>
       );
--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { nanoid } from 'nanoid';
 
 
-const Country = ({ name, capital, population, languages, flag }) => {
+const Country = ({ name, capital, population, languages, flag, weatherData }) => {
 
   let renderedLanguages = languages.map(language => {
     return <li key={nanoid()}>{language}</li>
   });
+
+  let weatherStuffs = null;
+
+  if(weatherData) {
+    weatherStuffs = (
+      <>
+        <p><b>Weather in {capital}</b></p>
+        <p><b>temperature:</b> {weatherData.temperature} celsius</p>
+        <img src={weatherData.icon} width='50' height='50' alt='Weather Icon'/>
+        <p><b>wind:</b> {weatherData.wind} mph direction {weatherData.wind_direction}</p>
+      </>
+    );
+  }
 
   return (
     <>
@@ -23,7 +36,8 @@ const Country = ({ name, capital, population, languages, flag }) => {
           }
         </ul>
       </div>
-      <img src={flag} width='300' height='220'/>
+      <img src={flag} width='300' height='220' alt='Country Flag'/>
+      { weatherStuffs }
     </>
   );
 };
