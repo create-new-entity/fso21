7c3a833e6a6a549a04d0e3d9239de3cfa498f34e
Pavel <imran.pavel@gmail.com>
2021-08-14 13:37:05 +0300

    5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,51 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+
+const Blog = ({blog}) => {
+  const [view, setView] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom:10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const viewButtonStyle = {
+    marginLeft: 10
+  };
+
+  console.log(blog);
+
+  const likeButtonHandler = () => {
+    console.log('Like Pressed');
+  };
+
+  const viewButtonHandler = () => {
+    setView(!view);
+  };
+
+  const detailsContent = () => {
+    if(!view) return null;
+    return (
+      <React.Fragment>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button onClick={likeButtonHandler}>like</button></p>
+        <p>{blog.author}</p>
+      </React.Fragment>
+    );
+  };
+  
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button style={viewButtonStyle} onClick={viewButtonHandler}>{ view ? 'hide' : 'view'}</button>
+      {
+        detailsContent()
+      }
+    </div>  
+  )
+};
 
 export default Blog
