204076bcdc0614c38aed6337d04c47d092355678
Pavel <imran.pavel@gmail.com>
2021-09-01 17:43:02 +0300

    Create notificationReducer.js

---
+++ part7/bloglist/src/reducers/notificationReducer.js
@@ -0,0 +1,30 @@
+
+export const createSetNotificationAction = (notification) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'CREATE_NOTIFICATION',
+      data: notification
+    });
+  };
+};
+
+export const createRemoveNotificationAction = () => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'REMOVE_NOTIFICATION'
+    });
+  };
+};
+
+const notificationReducer = (state = null, action) => {
+  switch(action.type) {
+  case 'CREATE_NOTIFICATION':
+    return action.data;
+  case 'REMOVE_NOTIFICATION':
+    return null;
+  default:
+    return state;
+  }
+};
+
+export default notificationReducer;
