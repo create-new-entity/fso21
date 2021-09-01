22cc6b38b3a3f45ad4af6831a2f7f8582dde9a9b
Pavel <imran.pavel@gmail.com>
2021-09-01 17:06:57 +0300

    Blogs reducer added

--- part7/bloglist/src/index.js
+++ part7/bloglist/src/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import store from './store';
+import App from './App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ part7/bloglist/src/reducers/blogsReducer.js
@@ -0,0 +1,65 @@
+import blogServices from '../services/blogs';
+
+export const createInitializeBlogsAction = () => {
+  return async (dispatch) => {
+    const blogs = await blogServices.getAll();
+    dispatch({
+      type: 'INITIALIZE_BLOGS',
+      data: blogs
+    });
+  };
+};
+
+export const createBlogLikedAction = (blog, blogId, userToken) => {
+  return async (dispatch) => {
+    const updatedBlog = await blogServices.updateABlogEntry(blog, userToken, blogId);
+    dispatch({
+      type: 'BLOG_LIKED',
+      data: updatedBlog
+    }
+    );
+  };
+};
+
+export const createAddNewBlogAction = (newBlog) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'ADD_NEW_BLOG',
+      data: newBlog
+    });
+  };
+};
+
+export const createRemoveBlogAction = (blogId, userToken) => {
+  return async (dispatch) => {
+    await blogServices.removeBlog(blogId, userToken);
+    dispatch({
+      type:'REMOVE_BLOG',
+      data: blogId
+    });
+  };
+};
+
+const blogsReducer = (state = [], action) => {
+  let newState, newBlogs, oldBlog;
+
+  switch(action.type){
+  case 'INITIALIZE_BLOGS':
+    return action.data;
+  case 'ADD_NEW_BLOG':
+    newState = [...state, action.data];
+    return newState;
+  case 'BLOG_LIKED':
+    newBlogs = state.map(blog => { return { ...blog }});
+    oldBlog = newBlogs.find(blog => blog.id.localeCompare(action.data.id) === 0);
+    oldBlog.likes = action.data.likes;
+    return newBlogs;
+  case 'REMOVE_BLOG':
+    newBlogs = state.filter(blog => blog.id.localeCompare(action.data) !== 0);
+    return newBlogs;
+  default:
+    return state;
+  }
+};
+
+export default blogsReducer;
