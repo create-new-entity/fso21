25a01fde24cda22c793eda2c06b6238ca45a3b83
Pavel <imran.pavel@gmail.com>
2021-09-01 18:30:58 +0300

    store written, all reducers written. App.js not changed yet.

--- part7/bloglist/src/index.js
+++ part7/bloglist/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
-import store from './store';
+// import store from './store';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
