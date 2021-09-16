da650838ceba2fc9101018f5e0ec575bb37d5807
Pavel <imran.pavel@gmail.com>
2021-09-16 13:04:59 +0300

    8.4

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -108,7 +108,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks: [Book!]!,
+    allBooks(author: String): [Book!]!,
     allAuthors: [Author!]!
   }
 `
@@ -122,7 +122,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_, args) => {
+      if(args && args.author) return books.filter(book => book.author === args.author);
+      return books;
+    },
     allAuthors: () => authors
   }
 }
