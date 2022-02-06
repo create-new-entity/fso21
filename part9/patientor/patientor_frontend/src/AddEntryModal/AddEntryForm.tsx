3e73a4b3d4559472c692a3992100ba4e0a3e09f3
Pavel <imran.pavel@gmail.com>
2022-02-06 09:35:29 +0200

    Diagnosis selection option added

--- part9/patientor/patientor_frontend/src/AddEntryModal/AddEntryForm.tsx
+++ part9/patientor/patientor_frontend/src/AddEntryModal/AddEntryForm.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { Formik, Form } from 'formik';
-import { HealthCheckRating } from './../types';
-
-import { DropDownField, TextField } from './FormField';
 import { Button } from 'semantic-ui-react';
+
+import { Diagnosis, HealthCheckRating } from './../types';
+import { DropDownField, TextField } from './FormField';
+import serviceFns from './../services';
+
 
 interface Props {
   onSubmit: () => void,
   onCancel: () => void
 }
 
+interface DropDownOptionsProps {
+  text: string,
+  value: string
+}
+
 const AddEntryForm = ({ onSubmit, onCancel } : Props) => {
+
+  const [diagnosisList, setDiagnosisList] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    void (async () => {
+      const newDiagnosisList = await serviceFns.getDiagnosisList();
+      setDiagnosisList(newDiagnosisList);
+    })();
+  }, []);
+
+  const getDiagnosisOptions = (): DropDownOptionsProps[] => {
+    return diagnosisList.map((diagnosis) => {
+      return {
+        text: diagnosis.code + ' : ' + diagnosis.name,
+        value: diagnosis.code
+      };
+    });
+  };
 
   const healthCheckEnumStuffs = Object.keys(HealthCheckRating);
   const healthCheckEnumValues = healthCheckEnumStuffs.slice(0, healthCheckEnumStuffs.length/2);
@@ -27,7 +51,8 @@ const AddEntryForm = ({ onSubmit, onCancel } : Props) => {
           {
             description: '',
             specialist: '',
-            healthCheckRating: HealthCheckRating.Healthy
+            healthCheckRating: HealthCheckRating.Healthy,
+            diagnosisCodes: []
         }
       }
       onSubmit={onSubmit}
@@ -42,20 +67,34 @@ const AddEntryForm = ({ onSubmit, onCancel } : Props) => {
                 name='description'
                 label='Description'
                 placeholder='Description'
+                fluid
               />
 
               <TextField
                 name='specialist'
                 label='Specialist'
                 placeholder='Specialist'
+                fluid
               />
-              
+
               <DropDownField
                 name='healthCheckRating'
                 label='Health Check Rating'
                 options={healthCheckDropDownOptions}
                 placeholder={HealthCheckRating[HealthCheckRating.Healthy]}
               />
+
+              <DropDownField
+                name='diagnosisCodes'
+                label='Diagnoses'
+                options={getDiagnosisOptions()}
+                placeholder='Diagnoses'
+                fluid
+                multiple
+                search
+                selection
+              />
+
               <Button onClick={onCancel}>Cancel</Button>
             </Form>
           );
--- part9/patientor/patientor_frontend/src/AddEntryModal/FormField.tsx
+++ part9/patientor/patientor_frontend/src/AddEntryModal/FormField.tsx
@@ -10,29 +10,34 @@ interface DropDownProps {
     text: string,
     value: string
   }[],
-  placeholder: string
+  placeholder: string,
+  fluid?: boolean,
+  multiple?: boolean,
+  search?: boolean,
+  selection?: boolean
 }
 
 interface TextFieldProps {
   name: string,
   label: string,
-  placeholder: string
+  placeholder: string,
+  fluid?: boolean
 }
 
-export const DropDownField = ({ name, label, options, placeholder }: DropDownProps) => {
+export const DropDownField = ({ name, label, ...props }: DropDownProps) => {
   return (
     <Form.Field>
       <label>{label}</label>
-      <Field name={name} component={Dropdown} options={options} placeholder={placeholder}/>
+      <Field name={name} component={Dropdown} {...props}/>
     </Form.Field>
   );
 };
 
-export const TextField = ({ name, label, placeholder }: TextFieldProps) => {
+export const TextField = ({ name, label, placeholder, ...props }: TextFieldProps) => {
   return (
     <Form.Field>
       <label>{label}</label>
-      <Field name={name} placeholder={placeholder} component={Input}/>
+      <Field name={name} placeholder={placeholder} component={Input} {...props}/>
     </Form.Field>
   );
 };
