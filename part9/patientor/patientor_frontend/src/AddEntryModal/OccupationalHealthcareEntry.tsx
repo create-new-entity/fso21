c549face040babefc8b52f9d77aa4c2266392c05
Pavel <imran.pavel@gmail.com>
2022-02-13 13:00:07 +0200

    Refactoring ErrorMessage

--- part9/patientor/patientor_frontend/src/AddEntryModal/OccupationalHealthcareEntry.tsx
+++ part9/patientor/patientor_frontend/src/AddEntryModal/OccupationalHealthcareEntry.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Field } from "formik";
+import { ErrorMessage, Field } from "formik";
 import { Segment, Form } from "semantic-ui-react";
 import { TextField } from "./FormField";
 
@@ -13,22 +13,29 @@ const OccupationalHealthcareEntry = () => {
         placeholder="Employer Name"
         component={TextField}
       />
+      <ErrorMessage name='employerName'/>
+
       <Form.Field>
         <label>Sick Leave</label>
         <Form.Field>
           <Segment>
+
             <Field
                 name="sickLeave.startDate"
                 label="Start Date"
                 placeholder="Start Date"
                 component={TextField}
             />
+            <ErrorMessage name="sickLeave.startDate"/>
+            
             <Field
               name="sickLeave.endDate"
               label="End Date"
               placeholder="End Date"
               component={TextField}
             />
+            <ErrorMessage name="sickLeave.endDate"/>
+            
           </Segment>
         </Form.Field>
       </Form.Field>
